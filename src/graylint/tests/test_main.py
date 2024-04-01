1ec7b3f9371eaeeb0c3d84a85a7c6d65526cbdec
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-04-01 22:33:34 +0300

    Adapt `test_main_retval` for Graylint

--- src/graylint/tests/test_main.py
+++ src/graylint/tests/test_main.py
@@ -2,49 +2,25 @@
 
 from pathlib import Path
 from textwrap import dedent
-from unittest.mock import Mock, patch, DEFAULT
-
-import pytest
+from unittest.mock import Mock, patch
 
 import darker
-from darkgraylib.utils import TextDocument
+import pytest
+
 from graylint.__main__ import main
 
 
 @pytest.mark.kwparametrize(
-    dict(check=False, changes=False, lintfail=False),
-    dict(check=False, changes=False, lintfail=True, expect_retval=1),
-    dict(check=False, changes=True, lintfail=False),
-    dict(check=False, changes=True, lintfail=True, expect_retval=1),
-    dict(check=True, changes=False, lintfail=False),
-    dict(check=True, changes=True, lintfail=True, expect_retval=1),
-    expect_retval=0,
+    dict(numfails=0, expect_retval=0),
+    dict(numfails=1, expect_retval=1),
+    dict(numfails=2, expect_retval=1),
 )
-def test_main_retval(git_repo, check, changes, lintfail, expect_retval):
-    """main() return value is correct based on --check, reformatting and linters"""
-    git_repo.add({"a.py": ""}, commit="Initial commit")
-    format_edited_parts = Mock()
-    format_edited_parts.return_value = (
-        [
-            (
-                Path("/dummy.py"),
-                TextDocument.from_lines(["old"]),
-                TextDocument.from_lines(["new"]),
-            )
-        ]
-        if changes
-        else []
-    )
-    run_linters = Mock(return_value=lintfail)
-    check_arg_maybe = ["--check"] if check else []
-    with patch.multiple(
-        "darker.__main__",
-        format_edited_parts=format_edited_parts,
-        modify_file=DEFAULT,
-        run_linters=run_linters,
-    ):
+def test_main_retval(numfails, expect_retval):
+    """main() return value is correct based on linter results."""
+    with patch("graylint.__main__.run_linters", Mock(return_value=numfails)):
+        # end of test setup
 
-        retval = main(check_arg_maybe + ["a.py"])
+        retval = main(["a.py"])
 
     assert retval == expect_retval
 
