2d22370960502eb27cf89c1049589e7445bba890
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-01-08 23:05:29 +0200

    feat: GitHub output plugin

---
+++ src/graylint/output/github.py
@@ -0,0 +1,17 @@
+"""GitHub output plugin for Graylint."""
+
+from graylint.linting import LinterMessage, MessageLocation
+from graylint.output.base import OutputPlugin
+
+
+class GitHubOutputPlugin(OutputPlugin):
+    """Output plugin for GitHub message annotations."""
+
+    def output(self, location: MessageLocation, message: LinterMessage) -> None:
+        """Output a message in the GitHub message annotation format."""
+        print("::error", end=" ", file=self._stream)
+        print(f"file={location.path}", end=",", file=self._stream)
+        print(f"line={location.line}", end=",", file=self._stream)
+        if location.column:
+            print(f"col={location.column}", end=",", file=self._stream)
+        print(f"title={message.linter}::{message.description}", file=self._stream)
