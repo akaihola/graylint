0349ff9add05485442f2ef3afb07c64b71da1ff9
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-01-09 08:58:33 +0200

    feat: add -L / --lint option without add_arg() helper

--- src/graylint/command_line.py
+++ src/graylint/command_line.py
@@ -1,10 +1,8 @@
 """Command line parsing for the ``graylint`` binary"""
 
 from argparse import ArgumentParser
-from typing import Any, Optional
 
 import darkgraylib.command_line
-from darkgraylib.command_line import add_parser_argument
 from graylint import help as hlp
 from graylint.version import __version__
 
@@ -22,12 +20,10 @@ def make_argument_parser(require_src: bool) -> ArgumentParser:
         hlp.DESCRIPTION,
         "Read Graylint configuration from `PATH`. Note that linters run by Graylint"
         " won't read this configuration file.",
-        __version__
+        __version__,
     )
 
-    def add_arg(help_text: Optional[str], *name_or_flags: str, **kwargs: Any) -> None:
-        kwargs["help"] = help_text
-        parser.add_argument(*name_or_flags, **kwargs)
-
-    add_arg(hlp.LINT, "-L", "--lint", action="append", metavar="CMD", default=[])
+    parser.add_argument(
+        "-L", "--lint", action="append", metavar="CMD", default=[], help=hlp.LINT
+    )
     return parser
