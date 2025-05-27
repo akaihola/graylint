d1197edb6657db0aba0cc11060d10c1fea393e6a
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-05-27 23:19:44 +0300

    feat(action): "with" option to install extra packages

--- action/main.py
+++ action/main.py
@@ -1,12 +1,20 @@
 """Graylint GitHub action implementation"""
 
+from __future__ import annotations
+
 import os
+import re
 import shlex
 import sys
 from pathlib import Path
 from subprocess import PIPE, STDOUT, run  # nosec
+from typing import TYPE_CHECKING
 
 from pip_requirements_parser import parse_reqparts_from_string
+
+if TYPE_CHECKING:
+    from pip_requirements_parser import RequirementParts
+
 
 LINTER_WHITELIST = {"flake8", "pylint", "mypy"}
 ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
@@ -16,6 +24,7 @@ OPTIONS = os.getenv("INPUT_OPTIONS", default="")
 SRC = os.getenv("INPUT_SRC", default="")
 VERSION = os.getenv("INPUT_VERSION", default="")
 LINT = os.getenv("INPUT_LINT", default="")
+WITH = os.getenv("INPUT_WITH", default="")
 REVISION = os.getenv("INPUT_REVISION") or os.getenv("INPUT_COMMIT_RANGE") or "HEAD^"
 
 run([sys.executable, "-m", "venv", str(ENV_PATH)], check=True)  # nosec
@@ -29,10 +38,18 @@ if VERSION:
     else:
         req[0] += f"=={VERSION}"
 linter_options = []
-for requirement_string in LINT.split(","):
-    if not requirement_string.strip():
-        continue
-    linter_requirement = parse_reqparts_from_string(requirement_string).requirement
+
+
+def split_requirements_string(requirements_string: str) -> list[RequirementParts]:
+    """Split requirements string into parts."""
+    return [
+        parse_reqparts_from_string(part.strip()).requirement
+        for part in re.split(r"\s*[,\s]\s*(?=[a-zA-Z_])", requirements_string)
+        if part.strip()
+    ]
+
+
+for linter_requirement in split_requirements_string(LINT):
     linter = linter_requirement.name
     if linter not in LINTER_WHITELIST:
         raise RuntimeError(
@@ -40,6 +57,7 @@ for requirement_string in LINT.split(","):
         )
     req.append(str(linter_requirement))
     linter_options.extend(["--lint", linter])
+req.extend(str(r) for r in split_requirements_string(WITH))
 
 pip_proc = run(  # nosec
     [str(ENV_BIN / "python"), "-m", "pip", "install"] + req,
